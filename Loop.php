e04620fe87692970721b653a6cabc3c65f896b23
AhmadRyahnah <ahmad.riahna@gmail.com>
2022-01-11 05:39:45 +0200

    loop

--- Loop.php
+++ Loop.php
@@ -34,37 +34,61 @@
 
 
     echo "EX3<br>";
-    $letter = "A";
-    // echo ++$letter;
-    $n = 5;
-    for ($i = 0; $i < $n-1; $i++) {
-        for ($j = $n; $j > 0; $j--) {
-            echo $letter;
-        }
+
+    $alpha = range('A', 'Z');
+    $k = 4;
+    for ($i = 0; $i <= 4; $i++) {
+        for ($j = $i; $j > 0; $j--)
+            echo $alpha[$j];
+        for ($k = 0; $k <= 4 - $i; $k++)
+            echo $alpha[$k];
         echo "<br>";
     }
 
 
 
-
-
-
     echo "<br>EX4<br>";
 
-    echo "EX5";
-    echo "EX6";
-    echo "EX7";
-    echo "EX8";
-    echo "EX9<br>";
+    echo "<br>EX5<br>";
+    echo "<br>EX6<br>";
+
+    function factorial($n)
+    {
+        $factorial = 1;
+        for ($i = $n; $i >= 1; $i--) {
+            $factorial *= $i;
+        }
+        return $factorial;
+    }
+    echo factorial(5);
+
+    
+    echo "<br>EX7<br>";
+    $S1 = 0;
+    $S2 = 1;
+    $n = 15;
+    echo $S1 . " , ";
+    echo $S2 . " , ";
+    for ($i = 2; $i < $n; $i++) {
+        $Sn = $S1 + $S2;
+        $S1 = $S2;
+        $S2 = $Sn;
+        echo $Sn . " , ";
+    }
+
+    echo "<br>EX8<br>";
+    echo "<br>EX9<br>";
+    echo "<table border=1>";
     for ($i = 1; $i <= 6; $i++) {
         echo "<tr><br>";
         for ($j = 1; $j <= 5; $j++) {
-            echo "<td>$i * $j = " . $i * $j . " ...... </td>";
+            echo "<td>$i * $j = " . $i * $j . "</td>";
         }
         echo "</tr>";
     }
-    echo "<br>EX10";
-    echo "EX11";
+    echo "</table>";
+    echo "<br>EX10<br>";
+    echo "<br>EX11<br>";
     echo "<br>EX11<br>";
 
     $n = 5;
@@ -78,32 +102,26 @@
         echo "<br>";
     }
     echo "<br>EX12<br>";
-    $n = 5;
-    $string = "";
-
-    for ($i = 1; $i <= $n; $i++) {
-
-        for ($j = $n; $j > $i; $j--) {
-            echo " A";
-        }
-
-        for ($k = 0; $k < $i * 2 - 1; $k++) {
-            echo  " B";
-        }
+    echo "<h3>";
+    $alpha = range('A', 'Z');
+    $k = 0;
+    for ($i = 0; $i <= 4; $i++) {
+        for ($j = 4; $j > $i; $j--)
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+        for ($k = 0; $k <= $i; $k++)
+            echo "&nbsp; " . $alpha[$k] . "&nbsp;&nbsp;&nbsp; ";
         echo "<br>";
     }
+    for ($i = 0; $i <= 3; $i++) {
+        for ($j = 0; $j < $i; $j++)
+            echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+        for ($k = 3; $k >= $i; $k--)
+            echo "&nbsp; &nbsp;" . $alpha[3 - $k] . "&nbsp;&nbsp;&nbsp;&nbsp;";
 
-    for ($i = 1; $i <= $n - 1; $i++) {
-
-        for ($j = 0; $j < $i; $j++) {
-            echo " ";
-        }
-
-        for ($k = ($n - $i) * 2 - 1; $k > 0; $k--) {
-            echo  " * ";
-        }
         echo "<br>";
     }
+    echo "</h3>";
+
     ?>
 </body>
 
